91f93b6ec4303cb9a03993adc5f0e8702f245e9d
Chan Haeng Lee <elt@Chanui-MacBookPro.local>
2019-11-18 16:05:16 +0900

    change code block

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 // require('normalize.css');
 require('./src/global.css');
-require('prismjs/themes/prism.css');
+// require('prismjs/themes/prism.css');
 require('prismjs/plugins/line-numbers/prism-line-numbers.css');
 
 // const { initI18n } = require('./src/i18n');
