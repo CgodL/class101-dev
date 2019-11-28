42cff094c2594d41fa362274aacc4af4efebe220
Chan Haeng Lee <elt@Chanui-MacBookPro.local>
2019-11-28 17:11:44 +0900

    add calendar

---
+++ src/templates/til.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+// tslint:disable-next-line: class-name
+export default class til extends Component {
+  public render() {
+    return (
+      // tslint:disable-next-line: jsx-self-close
+      <div></div>
+    );
+  }
+}
